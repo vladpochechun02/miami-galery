1b0c38292df3e0a8507a8e4111d633a997b74acb
Vladyslav <kpigoodvlad@gmail.com>
2023-02-17 21:29:09 +0200

    Created skider with cool animations

--- ready-html/js/app.js
+++ ready-html/js/app.js
@@ -1,3 +1,25 @@
 const sliderMain = new Swiper('.slider_main',{
-    
+    freeMode: true,
+    centeredSlides: true,
+    mousewheel: true,
+    parallax: true,
+    breakpoints: {
+        0:{
+            slidesPerView: 2.5,
+            spaceBetween: 20
+        },
+        680:{
+            slidesPerView: 3.5,
+            spaceBetween: 60
+        }
+    }
 })
+
+const sliderBg = new Swiper('.slider_bg',{
+    centeredSlides: true,
+    parallax: true,
+    spaceBetween: 60,
+    slidesPerView: 3.5
+})
+
+sliderMain.controller.control = sliderBg
